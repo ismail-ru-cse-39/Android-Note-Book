0ca1182bfb8abac60656072728caec5163a8bc80
Ismail Hossain <ismailhossain@Ismails-MacBook-Pro.local>
2023-01-31 23:17:46 +0600

    Model Package and data classes are created.

---
+++ app/src/main/java/com/example/androidnotebook/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.androidnotebook.model
+
+data class User(
+    val __v: Int,
+    val _id: String,
+    val email: String,
+    val updatedAt: String,
+    val userName: String
+)
