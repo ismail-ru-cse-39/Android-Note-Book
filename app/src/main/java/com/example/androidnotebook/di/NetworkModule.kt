00da7c821bdaff1a9fe3bee94a703539d53b208a
Ismail Hossain <ismailhossain@Ismails-MacBook-Pro.local>
2023-02-01 00:49:00 +0600

    Build error related to kotlin version.

--- app/src/main/java/com/example/androidnotebook/di/NetworkModule.kt
+++ app/src/main/java/com/example/androidnotebook/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.example.androidnotebook.di
 
 import com.example.androidnotebook.api.UserAPI
 import com.example.androidnotebook.utils.Constants
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -11,6 +12,7 @@ import retrofit2.create
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
+@Module
 class NetworkModule {
     @Singleton
     @Provides
